0f16336785865de9716bbaa50b5169b4fc780520
StageGuard <1355416608@qq.com>
2023-01-01 11:01:14 +0800

    fix local dependence

--- app/src/main/java/me/stageguard/aruku/ui/page/home/HomeTopAppBar.kt
+++ app/src/main/java/me/stageguard/aruku/ui/page/home/HomeTopAppBar.kt
@@ -1,32 +1,15 @@
 package me.stageguard.aruku.ui.page.home
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.AddCircleOutline
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import me.stageguard.aruku.R
-import me.stageguard.aruku.ui.LocalBot
 import me.stageguard.aruku.ui.page.home.account.AccountAvatar
 import me.stageguard.aruku.ui.theme.ArukuTheme
-import me.stageguard.aruku.ui.theme.ColorAccountOffline
-import me.stageguard.aruku.ui.theme.ColorAccountOnline
-import me.stageguard.aruku.util.stringResC
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -70,6 +53,7 @@ fun HomeTopAppBar(
 @Composable
 fun HomeTopAppBarPreview() {
     val expanded = remember { mutableStateOf(true) }
+    val state = remember { mutableStateOf(AccountState.Default) }
     ArukuTheme {
         HomeTopAppBar(
             botList = listOf(
@@ -78,7 +62,7 @@ fun HomeTopAppBarPreview() {
                 BasicAccountInfo(1145141919, "WhichWho", null),
             ),
             botListExpanded = expanded,
-            state = mutableStateOf(AccountState.Default),
+            state = state,
 //            showAvatarProgressIndicator = true,
             onAvatarClick = { expanded.value = !expanded.value },
             onSwitchAccount = {},
