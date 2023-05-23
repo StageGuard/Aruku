dc225d9f38706297bf1d3423d04dd7d648119797
StageGuard <1355416608@qq.com>
2023-05-23 08:37:09 +0800

    mask password in log

--- app/src/main/java/me/stageguard/aruku/service/parcel/AccountLoginData.kt
+++ app/src/main/java/me/stageguard/aruku/service/parcel/AccountLoginData.kt
@@ -14,4 +14,19 @@ data class AccountLoginData(
     var statHeartbeatPeriodMillis: Long,
     var autoReconnect: Boolean,
     var reconnectionRetryTimes: Int
-) : Parcelable
+) : Parcelable {
+    override fun toString(): String {
+        return buildString {
+           append("AccountLoginData(")
+           append("accountNo=$accountNo, ")
+           append("passwordMd5=<masked>, ")
+           append("protocol=$protocol, ")
+           append("heartbeatStrategy=$heartbeatStrategy, ")
+           append("heartbeatPeriodMillis=$heartbeatPeriodMillis, ")
+           append("heartbeatTimeoutMillis=$heartbeatTimeoutMillis,")
+           append("statHeartbeatPeriodMillis=$statHeartbeatPeriodMillis,")
+           append("autoReconnect=$autoReconnect,")
+           append("reconnectionRetryTimes=$reconnectionRetryTimes)")
+        }
+    }
+}
